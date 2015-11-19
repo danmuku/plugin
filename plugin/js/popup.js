98d921fe555ac4528c7e8d7ee9cb92e565bfea88
veryyoung <codingyoung@gmail.com>
2015-11-19 15:33:31 +0800

    根据是否打开来改变logo

---
+++ plugin/js/popup.js
@@ -0,0 +1,38 @@
+	
+/**
+ * Fill in content into the popup on load
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  // Set up toggle checkbox for danmaco mode
+  getOption_('danmacoOn', false, function(item) {
+    var danmacoOnCheckbox = document.getElementById('danmaco-checkbox');
+    console.log(danmacoOnCheckbox);
+    danmacoOnCheckbox.addEventListener('click', toggleDanmaco, false);
+    var danmacoEnabled = item.danmacoOn;
+    if (danmacoEnabled) {
+      danmacoOnCheckbox.setAttribute('checked', '');
+    }
+  });
+
+});
+
+
+
+
+function toggleDanmaco() {
+  getOption_('danmacoOn', false, function(item) {
+    setOption_('danmacoOn', !item.danmacoOn);
+  });
+}
+
+function getOption_(opt, defaultOpt, callback) {
+  var details = {};
+  details[opt] = defaultOpt;
+  return chrome.storage.sync.get(details, callback);
+}
+
+function setOption_(opt, value) {
+  var details = {};
+  details[opt] = value;
+  return chrome.storage.sync.set(details);
+}
