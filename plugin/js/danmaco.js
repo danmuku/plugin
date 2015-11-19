5abd4c3ccb38d816eca8f3d2d73bbea8e63506fb
veryyoung <codingyoung@gmail.com>
2015-11-19 17:38:52 +0800

    use inner firebase

--- plugin/js/danmaco.js
+++ plugin/js/danmaco.js
@@ -1,64 +1,58 @@
-var urlLocation = location.href;
+var firebase;
 
-if(urlLocation.charAt(urlLocation.length - 1) === '#'){
-	urlLocation = urlLocation.substr(0, urlLocation.length-1);
-}
-
-if(urlLocation.charAt(urlLocation.length - 1) === '/'){
-	urlLocation = urlLocation.substr(0, urlLocation.length-1);
-}
 chrome.storage.sync.get({danmacoOn: false}, function(item) {
-  var danmacoOn = item.danmacoOn;
+    var danmacoOn = item.danmacoOn;
 
-  // danmu only when danmacoOn
-  if(danmacoOn){
-  // create div
-  var danmacoDiv=$('<div class="danmako_screen"><div class="s_dm"><div class="mask"></div>\
-    <div class="s_show"></div></div><div class="send">\
-    <div class="s_con"><input type="text" class="s_txt"/>\
-    <input type="button" class="s_btn" value="发表评论"/></div></div></div>');            
-  danmacoDiv.appendTo('body');   
+    // danmu only when danmacoOn
+    if (danmacoOn) {
+        // create div
+        var danmacoDiv = $('<div class="danmako_screen"><div class="s_dm"><div class="mask"></div>\
+            <div class="s_show"></div></div><div class="send">\
+            <div class="s_con"><input type="text" class="s_txt"/>\
+            <input type="button" class="s_btn" value="发表评论"/></div></div></div>');
+        danmacoDiv.appendTo('body');
 
-  var firebase;
-  $.ajaxSetup({cache: true});
+        // build firebase
+        var urlLocation = location.href;
 
-  // load firebase.js
-  $.getScript("https://cdn.firebase.com/js/client/2.2.1/firebase.js", function(){
-    firebase = new Firebase('https://dazzling-fire-9662.firebaseio.com/'+ window.btoa(urlLocation));
-    firebase.on('child_added',
-        function (snapshot) {
+        if (urlLocation.charAt(urlLocation.length - 1) === '#') {
+            urlLocation = urlLocation.substr(0, urlLocation.length - 1);
+        }
+
+        if (urlLocation.charAt(urlLocation.length - 1) === '/') {
+            urlLocation = urlLocation.substr(0, urlLocation.length - 1);
+        }
+
+        firebase = new Firebase('https://dazzling-fire-9662.firebaseio.com/' + window.btoa(urlLocation));
+
+        firebase.on('child_added',
+        function(snapshot) {
             var message = snapshot.val();
-            console.log(message);
             $(".s_show").append("<div>" + message + "</div>");
             init_screen();
         });
-    });
-  };
+    }
 
 });
 
-
-
-
-
 //发表评论
-$(".s_btn").click(function () {
+$(".s_btn").click(function() {
     post();
 });
 
-$(".s_txt").keydown(function () {
+$(".s_txt").keydown(function() {
     var code = window.event.keyCode;
     if (code == 13) {
         post();
     }
 });
 
-$(document).keydown(function(event){
-	var keyCode = event.keyCode;
-	if (keyCode == 13) {
+$(document).keydown(function(event) {
+    var keyCode = event.keyCode;
+    if (keyCode == 13) {
         post();
-    }else if(keyCode == 27){
-    	$(".danmako_screen").toggle(600);
+    } else if (keyCode == 27) {
+        $(".danmako_screen").toggle(600);
     }
 });
 
@@ -72,14 +66,11 @@ function post() {
     }
 }
 
-
-
-
 //初始化弹幕
 function init_screen() {
     var _top = 0;
 
-    $(".s_show").find("div").show().each(function () {
+    $(".s_show").find("div").show().each(function() {
         var _left = $(window).width() - $(this).width();
         var _height = $(window).height();
 
@@ -100,19 +91,19 @@ function init_screen() {
             color: getRandomColor()
         });
         $(this).animate({
-                left: "-" + _left + "px"
-            },
-            time,
-            function () {
+            left: "-" + _left + "px"
+        },
+        time,
+        function() {
 
-            });
+});
 
     });
 }
 
 //随机获取颜色值
 function getRandomColor() {
-    return '#' + (function (h) {
-            return new Array(7 - h.length).join("0") + h
-        })((Math.random() * 0x1000000 << 0).toString(16))
+    return '#' + (function(h) {
+        return new Array(7 - h.length).join("0") + h
+    })((Math.random() * 0x1000000 << 0).toString(16))
 }
