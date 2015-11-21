94b1211d55793e14da6b835c6523c9f37dfea438
veryyoung <codingyoung@gmail.com>
2015-11-21 16:10:35 +0800

    use firebaseLib

--- special/wechat/server.php
+++ special/wechat/server.php
@@ -57,7 +57,7 @@
      * @return void
      */
     protected function onText() {
-      $firebaseStub = new FirebaseStub("https://dazzling-fire-9662.firebaseio.com/sogou-hackathon2", '');
+      $firebaseStub = new FirebaseLib("https://dazzling-fire-9662.firebaseio.com/sogou-hackathon2", '');
       $firebaseStub->push("/", $this->getRequest('content'));
       $this->responseText('收到了文字消息：' . $this->getRequest('content') . "\r\n快到墙上看看吧！");
     }
