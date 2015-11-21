e4f5626345149367483d7310dcfd32196ec7717e
veryyoung <codingyoung@gmail.com>
2015-11-21 10:16:57 +0800

    set not valid

--- special/wechat/main.php
+++ special/wechat/main.php
@@ -1,15 +1,15 @@
  <?php
 
- include "wechat.class.php";
- 
- $options = array('token'=>'little_fresh_boys', //填写你设定的key
- 			'appid'=>'wxce6838b261a9f625', //填写高级调用功能的app id
- 			'appsecret'=>'d4624c36b6795d1d99dcf0547af5443d' //填写高级调用功能的密钥
- 		);
+include "wechat.class.php";
 
- $weObj = new Wechat($options);
- $weObj->valid();
- $type = $weObj->getRev()->getRevType();
+$options = array('token'=>'little_fresh_boys', //填写你设定的key
+ 		'appid'=>'wxce6838b261a9f625', //填写高级调用功能的app id
+ 		'appsecret'=>'d4624c36b6795d1d99dcf0547af5443d' //填写高级调用功能的密钥
+ 	);
+
+$weObj = new Wechat($options);
+// $weObj->valid();
+$type = $weObj->getRev()->getRevType();
 
 switch($type) {
 	case Wechat::MSGTYPE_TEXT:
