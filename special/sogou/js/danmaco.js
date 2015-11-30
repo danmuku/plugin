bf587c901745624b6ef23425cdbde0c62ce68503
veryyoung <codingyoung@gmail.com>
2015-11-30 22:42:02 +0800

    修复文字可能贴下边的bug

--- special/sogou/js/danmaco.js
+++ special/sogou/js/danmaco.js
@@ -1,9 +1,9 @@
 
-var firebase = new Firebase('https://dazzling-fire-9662.firebaseio.com/'+ 'sogou-hackathon2');
+var firebase = new Firebase('https://dazzling-fire-9662.firebaseio.com/' + 'sogou-hackathon2');
 
 firebase.on('child_added', function (snapshot) {
     var message = snapshot.val();
-    if(message.length > 35){
+    if (message.length > 35) {
         message = message.substr(0, 35) + "...";
     }
     $(".s_show").append("<div>" + message + "...</div>");
@@ -13,26 +13,23 @@ firebase.on('child_added', function (snapshot) {
 //初始化弹幕
 function refresh_screen() {
     var _width = $(window).width();
-
     var _height = $(window).height();
-    var _top = Math.random()*(_height);
 
-    if (_top > _height - 100) {
-        _top = 20;
+    var _top = Math.random() * (_height);
+    if (_top > _height - 30) {
+        _top += 30;
     }
 
     var time = Math.random() * 20000 + 10000;
-    
-
+ 
     //设定文字的初始化位置
     $(".s_show").children("div:last-child").css({
         left: _width,
         top: _top,
         color: getRandomColor()
     });
-   
 
-    $(".s_show").children("div:last-child").animate({left: "-" + _width + "px"}, time, function() {
+    $(".s_show").children("div:last-child").animate({ left: "-" + _width + "px" }, time, function () {
         $(this).remove();
     });
 }
@@ -40,6 +37,6 @@ function refresh_screen() {
 //随机获取颜色值
 function getRandomColor() {
     return '#' + (function (h) {
-            return new Array(7 - h.length).join("0") + h
-        })((Math.random() * 0x1000000 << 0).toString(16))
+        return new Array(7 - h.length).join("0") + h
+    })((Math.random() * 0x1000000 << 0).toString(16))
 }
